49ab9f272e0bda166b2252004cf17e2da9199019
William Soukkachang <WSoukkachang@yahoo.com>
2019-04-06 13:27:00 -0500

    created changeSeleced and filterCards methods, refactored TopBar and Header using styled components

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import Tabs from './Tabs';
 import Cards from './Cards';
 
@@ -11,23 +10,30 @@ export default class Content extends Component {
     super(props);
     this.state = {
       selected: 'all',
-      tabs: tabData,
-      cards: cardData
+      tabs: [],
+      cards: []
     };
   }
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
-  }
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    })
+  };
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({
+      selected: tab
+    })
   };
 
   filterCards = () => {
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
-      by using it to filter out our cards for when a tab is selcted
+      by using it to filter out our cards for when a tab is selected
       
       Notice that we're passing this function to our <Cards /> component below.
       This function returns an array of cards, so we can just pass it down as such.
@@ -37,10 +43,19 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+
+    if (this.state.selected === 'all') {
+      return this.state.cards
+    }
+    else {
+      return (
+        this.state.cards.filter(card => card.tab === this.state.selected)
+      )
+    }
   };
 
   render() {
+
     return (
       <div className="content-container">
       
@@ -52,8 +67,8 @@ export default class Content extends Component {
 
         <Tabs 
           tabs={this.state.tabs} 
-          selectedTab={this.state.changeSelected}
-          selectTabHandler={this.state.filterCards}
+          selectedTab={this.state.selected}
+          selectTabHandler={this.state.changeSelected}
         />
         <Cards cards={this.filterCards()} />
       </div>
