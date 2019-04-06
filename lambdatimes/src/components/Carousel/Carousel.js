cf5d4bceaa8fb348bb91da9ab885e703657838b2
William Soukkachang <WSoukkachang@yahoo.com>
2019-04-06 14:07:10 -0500

    working on carousel stretch

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
+
 // Complete this Carousel 
 export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
-    }
+      carousel: carouselData,
+      next: data.next, // - stretch
+      prev: data.previous } // - stretch
   }
+
   componentDidMount(){
+    this.setState({
+      carouselData: carouselData
+    })
+  };
+
+  leftClick = e => {
+    if (this.state.prev === null) {
+      e.preventDefault();
+    }
+
+    else {
+      this.setState(this.state.prev);
+    }
 
   }
 
-  leftClick = () => {
-
-  }
-
-  rightClick = () => {
+  rightClick = e => {
+    if (this.state.next === null) {
+      e.preventDefault();
+    }
+    
+    else {
+      this.setState(this.state.next);
+    }
 
   }
 
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Tabs from './Tabs';
 import Cards from './Cards';
+import Carousel from '../Carousel/Carousel'; // - carousel stretch
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
@@ -71,6 +72,7 @@ export default class Content extends Component {
           selectedTab={this.state.selected}
           selectTabHandler={this.changeSelected}
         />
+        <Carousel /> 
         <Cards cards={this.filterCards()} />
       </div>
     );
